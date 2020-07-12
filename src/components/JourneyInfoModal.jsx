44d7932b5db77bd2381a80811788919eceed591d
Cassady <cassady801@gmail.com>
2020-07-12 12:38:23 -0600

    Remove mongo  db, finished modal

--- src/components/JourneyInfoModal.jsx
+++ src/components/JourneyInfoModal.jsx
@@ -2,52 +2,77 @@ import React, { Component } from 'react';
 import { Modal, Button, ButtonToolbar } from 'rsuite';
 
 export default class JourneyInfoModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        };
+
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
     }
 
-    this.close = this.close.bind(this);
-    this.open = this.open.bind(this);
-  }
+    close() {
+        this.setState({ show: false });
+    }
+    open() {
+        this.setState({ show: true });
+    }
 
-  close() {
-    this.setState({ show: false });
-}
-open() {
-    this.setState({ show: true });
-}
-  
-  render() {
+    render() {
+        return (
+            <div className="modal-container">
+                <ButtonToolbar>
+                    <Button onClick={this.open}>How to Use</Button>
+                </ButtonToolbar>
 
-     return(
-      <div className="modal-container">
-      <ButtonToolbar>
-          <Button onClick={this.open}> Open</Button>
-      </ButtonToolbar>
-
-      <Modal show={this.state.show} onHide={this.close}>
-          <Modal.Header>
-              <Modal.Title>Modal Title</Modal.Title>
-          </Modal.Header>
-          <Modal.Body></Modal.Body>
-          <Modal.Footer>
-              <Button
-                  onClick={this.close}
-                  appearance="primary"
-              >
-                  Ok
-              </Button>
-              <Button
-                  onClick={this.close}
-                  appearance="subtle"
-              >
-                  Cancel
-              </Button>
-          </Modal.Footer>
-      </Modal>
-  </div>
-       )
-   }
+                <Modal
+                    className="w-100 p-3"
+                    show={this.state.show}
+                    onHide={this.close}
+                >
+                    <Modal.Header>
+                        <Modal.Title
+                            style={{
+                                fontFamily: 'Roboto Condensed, sans-serif',
+                                fontSize: '1.5rem',
+                            }}
+                        >
+                            What is Journey?
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body
+                        style={{
+                            fontFamily: 'Roboto Condensed, sans-serif',
+                            fontSize: '1.2rem',
+                        }}
+                    >
+                        Journey is a travel app designed to make splitting the
+                        cost between friends/family easy!
+                        <hr />
+                        1. Add in the dates for your trip <br />
+                        2. Add and adjust the people going <br />
+                        3. Finally, enter the amount for your trip! <br />
+                        <br />
+                        ** Journey will split the amount of the trip by figuring
+                        out the cost per day, taking into account how many
+                        people are present.
+                        <br /> Perfect for trips where you may have people
+                        coming and going!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            onClick={this.close}
+                            style={{
+                                backgroundColor: '#7a5ad8',
+                                color: 'white',
+                            }}
+                        >
+                            Got It!
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
 }
